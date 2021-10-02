ebf2f6b7639aebd33b77686ef6d4cbd7c3340b48
unknown <rithwiksingh007@gmail.com>
2021-10-02 23:46:54 +0530

    Modified_Readme

--- src/components/TodoItemTask.js
+++ src/components/TodoItemTask.js
@@ -5,9 +5,9 @@ const TodoItemTask = ({taskId,assignedName, message, priority}) => {
 		<li className={`list-group-item ${'list-group-item-success'}`}>
 			<div className='d-flex justify-content-between'>
 				<span className='d-flex align-items-center'>Task-Id:-{taskId}</span>
-				<span className='d-flex align-items-center'>AssignedName:{assignedName}</span>
-                <span className='d-flex align-items-center'>Message:{message}</span>
-                <span className='d-flex align-items-center'>Priority:{priority}</span>
+				<span className='d-flex align-items-center'>AssignedName:-{assignedName}</span>
+                <span className='d-flex align-items-center'>Message:-{message}</span>
+                <span className='d-flex align-items-center'>Priority:-{priority}</span>
 				
 			</div>
 		</li>
